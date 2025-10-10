a1865697e7e926a3920a0919212956b0ebde3c84
smyyearslan <smyye.arslann05@gmail.com>
2025-10-10 17:34:08 +0300

    HMW-3 time complexity

---
+++ HOMEWORKS/HMW3-timecomplexity.cpp
@@ -0,0 +1,66 @@
+int fun(int n)
+{
+	int count=0;
+	for(int i=n;i>0;i/=2)
+		for(int j=0;j<i;j++)
+			count+=1;
+		return count;
+}
+// 1) TIME COMPLEXITY = O(n)
+
+int a=0,b=0;
+	for(i=0;i<N;i++){
+		a=a+rand();
+	}
+		for(j=0;j<M;j++){
+			b=b+rand();
+		}
+// 2)TIME COMPLEXITY = O(n+m)
+
+for(int i=n;i>0;i=i/2)
+{
+	for(int j=1;j<n;j=j*2)
+}
+		for(int k=0;k<n;k=k+2)
+}
+// 3)TIME COMPLEXITY = O(nlogn)???
+
+for(int i=0;i<n;i++){
+	i*=k;
+}
+// 4)TIME COMPLEXITY = O(log(n))
+
+int i,j,k=0;
+for(i=n/2;i<=n;i++){
+	for(j=2;j<=n;j=j*2)
+	{
+		k=k+n/2;
+	}
+}
+// 5)TIME COMPLEXITY = O(nlog(n))
+
+int a=0,i=N;
+	while(i>0){
+		a+=i;
+		i/=2;
+	}
+// 6)TIME COMPLEXITY = O(log(n))
+
+int value=0;
+	for(int i=0;i<n;i++)
+		for(int j=0;j<i;j++)
+		value+=1;
+// 7)TIME COMPLEXITY = O(n^2)
+
+
+
+
+
+
+
+
+
+
+
+
+
